11e94877e3ff1d908bd025189fd659e8ce6d1821
Edil Medeiros <jose.edil@gmail.com>
2024-11-23 09:58:53 -0300

    Update signet server parameters

--- test/setup.sh
+++ test/setup.sh
@@ -1,7 +1,7 @@
-wget https://bitcoincore.org/bin/bitcoin-core-26.0/bitcoin-26.0-x86_64-linux-gnu.tar.gz
-tar -xzvf bitcoin-26.0-x86_64-linux-gnu.tar.gz
-ln -s $PWD/bitcoin-26.0/bin/* /usr/local/bin/
-bitcoind -daemon -signet -signetchallenge=0014e465e43a5e6a013f5c5ec43eae826f57cbb3f83d -blocksonly=1 -addnode=35.209.148.157:12738
+wget https://bitcoincore.org/bin/bitcoin-core-28.0/bitcoin-28.0-x86_64-linux-gnu.tar.gz
+tar -xzvf bitcoin-28.0-x86_64-linux-gnu.tar.gz
+ln -s $PWD/bitcoin-28.0/bin/* /usr/local/bin/
+bitcoind -daemon -signet -signetchallenge=0014bdec02fe5ec499cc2cb52dc160230643a84dd118 -blocksonly=1 -addnode=146.190.155.176:28333
 while true; do
     blockcount=$(bitcoin-cli -signet getblockcount)
     if [[ $blockcount -ge 300 ]]; then
@@ -11,4 +11,4 @@ while true; do
         sleep 1
     fi
 done
-bitcoin-cli -signet invalidateblock 0000009b0e57d6f42727fe4047eaa9d6be25c82dbf19d55fda56c85ad6189d58
+bitcoin-cli -signet invalidateblock 0000027697cdb0347a40017a44e7b936be55a5217d122c570a2b68976baeff88
